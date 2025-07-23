a182c31115dc0093e9ea7ef08271a88206504d67
NicoMunoz909 <nicolas.munoz@outlook.es>
2025-07-23 14:56:15 -0500

    "Eliminar CFDI, isBackup and isDemo"

--- src/components/InventoryList/index.jsx
+++ src/components/InventoryList/index.jsx
@@ -1,44 +1,63 @@
-import formatDate from '../../utils/formatDates';
-import styles from './inventoryList.module.css'
+import formatDate from "../../utils/formatDates";
+import styles from "./inventoryList.module.css";
 import { TiDelete } from "react-icons/ti";
 
-const InventoryList = ( { style = {}, attributes, items = [], selectedIds = [], mode = undefined, onDelete = null, onSelect = null, onCancel = null } ) => {
-
+const InventoryList = ({
+  style = {},
+  attributes,
+  items = [],
+  selectedIds = [],
+  mode = undefined,
+  onDelete = null,
+  onSelect = null,
+  onCancel = null,
+}) => {
   const handleSelect = () => {
-    const selectedRows = Array.from(document.getElementsByName("select-item")).filter( (node) => node.checked).map( (node) => parseInt(node.id.split("-")[2]))
-    const pickedIds = selectedRows.map( (index) => {return items[index].id});
-    const pickedItems = selectedRows.map( (index) => {return items[index]});
-    onSelect(pickedItems, pickedIds)
-  }
+    const selectedRows = Array.from(document.getElementsByName("select-item"))
+      .filter((node) => node.checked)
+      .map((node) => parseInt(node.id.split("-")[2]));
+    const pickedIds = selectedRows.map((index) => {
+      return items[index].id;
+    });
+    const pickedItems = selectedRows.map((index) => {
+      return items[index];
+    });
+    onSelect(pickedItems, pickedIds);
+  };
 
   const renderButton = (index) => {
     switch (mode) {
       case "SHOW":
         return (
-        <button className={styles.deleteButton} onClick={() => onDelete(index)}>
-          <TiDelete />
-        </button>)
+          <button
+            className={styles.deleteButton}
+            onClick={() => onDelete(index)}
+          >
+            <TiDelete />
+          </button>
+        );
       case "PICKING":
         return (
           <>
             <label htmlFor={`select-item-${index}`}></label>
-            <input 
-            className={styles.selectBox} 
-            name="select-item"
-            id={`select-item-${index}`}
-            type="checkbox" 
-            defaultChecked={selectedIds.includes(items[index].id)}/>
+            <input
+              className={styles.selectBox}
+              name="select-item"
+              id={`select-item-${index}`}
+              type="checkbox"
+              defaultChecked={selectedIds.includes(items[index].id)}
+            />
           </>
-        )
+        );
       default:
         return null;
     }
-  }
+  };
 
   return (
     <div className={styles.container}>
       <div style={style} className={styles.tableContainer}>
-        <table className={styles.table} id='tabla-inventario'>
+        <table className={styles.table} id="tabla-inventario">
           <thead>
             <tr>
               {attributes.map((key) => (
@@ -47,40 +66,34 @@ const InventoryList = ( { style = {}, attributes, items = [], selectedIds = [], 
             </tr>
           </thead>
           <tbody>
-            {items.filter( (item) => !selectedIds.includes(item.id) ).map((item, index) => (
-              <tr key={index}>
-                <td>{index + 1}</td>
-                {Object.entries(item).map(([key, value]) =>
-                  {
+            {items
+              .filter((item) => !selectedIds.includes(item.id))
+              .map((item, index) => (
+                <tr key={index}>
+                  <td>{index + 1}</td>
+                  {Object.entries(item).map(([key, value]) => {
                     if (key !== "id") {
                       if (key === "fechaEntrada" || key === "fechaSalida") {
-                        return <td key={key}>{formatDate(value)}</td>
-                      } else if (key === 'isDemo' || key === 'isBackup') {
-                        return <td key={key}>{value ? 'SI' : 'NO'}</td>
+                        return <td key={key}>{formatDate(value)}</td>;
                       } else {
-                        return <td key={key}>{value}</td>
+                        return <td key={key}>{value}</td>;
                       }
                     }
-                  })
-                }
-                {mode &&
-                  <td>
-                    {renderButton(index)}
-                  </td>
-                }
-              </tr>
-            ))}
+                  })}
+                  {mode && <td>{renderButton(index)}</td>}
+                </tr>
+              ))}
           </tbody>
         </table>
       </div>
-      {mode === "PICKING" && 
-          <div className={styles.buttonsContainer}>
-            <button onClick={handleSelect} >SELECCIONAR</button>
-            <button onClick={onCancel}>CANCELAR</button>
-          </div>
-        }
+      {mode === "PICKING" && (
+        <div className={styles.buttonsContainer}>
+          <button onClick={handleSelect}>SELECCIONAR</button>
+          <button onClick={onCancel}>CANCELAR</button>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default InventoryList
+export default InventoryList;
--- src/pages/EntriesPage/index.jsx
+++ src/pages/EntriesPage/index.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react';
-import styles from './entriesPage.module.css';
-import inventoryService from '../../services/inventoryService';
-import MainLayout from '../../layouts/MainLayout';
-import Input from '../../components/Input';
-import EnterButton from '../../components/EnterButton';
-import InventoryList from '../../components/InventoryList';
-import FinishButton from '../../components/FinishButton';
-import FormFields from '../../components/FormFields';
-import LoadingSpinner from '../../components/LoadingSpinner';
-import { useAlert } from '../../contexts/AlertContext';
+import { useState } from "react";
+import styles from "./entriesPage.module.css";
+import inventoryService from "../../services/inventoryService";
+import MainLayout from "../../layouts/MainLayout";
+import Input from "../../components/Input";
+import EnterButton from "../../components/EnterButton";
+import InventoryList from "../../components/InventoryList";
+import FinishButton from "../../components/FinishButton";
+import FormFields from "../../components/FormFields";
+import LoadingSpinner from "../../components/LoadingSpinner";
+import { useAlert } from "../../contexts/AlertContext";
 
 const EntriesPage = () => {
-  const [activeTab, setActiveTab] = useState("DATOS")
+  const [activeTab, setActiveTab] = useState("DATOS");
   const [inputs, setInputs] = useState({
     proveedor: "",
     ordenDeCompra: "",
     facturaCompra: "",
-    cfdi: "",
     almacen: "",
     sector: "",
     partNumber: "",
     descripcion: "",
     serialNumber: "",
-  })
-  const [checkboxes, setCheckboxes] = useState({
-    isBackup: false,
-    isDemo: false,
-  })
-  const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  });
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const tabs = [
     {
       key: "DATOS",
@@ -40,23 +35,14 @@ const EntriesPage = () => {
     },
     {
       key: "PART_NUMBER",
-      title: "Part Number"
-    }
-  ]
+      title: "Part Number",
+    },
+  ];
   const { showAlert } = useAlert();
 
   const handleTabChange = (tab) => {
-    setActiveTab(tab)
-    setInputs({...inputs, partNumber: "", descripcion: "", serialNumber: ""})
-    setCheckboxes({isBackup: false, isDemo: false})
-  }
-
-  const handleCheckboxChange = (e) => {
-    const {name, checked} = e.target
-    setCheckboxes((prevCheckboxes) => ({
-      ...prevCheckboxes,
-      [name]: checked,
-    }));
+    setActiveTab(tab);
+    setInputs({ ...inputs, partNumber: "", descripcion: "", serialNumber: "" });
   };
 
   const handleInputChange = (e) => {
@@ -65,48 +51,50 @@ const EntriesPage = () => {
       ...prevInputs,
       [name]: value,
     }));
-  }
+  };
 
   const handleSerialEntry = (e) => {
     e.preventDefault();
 
-    const serialExists = items.some(item => item.serialNumber === inputs.serialNumber);
+    const serialExists = items.some(
+      (item) => item.serialNumber === inputs.serialNumber
+    );
 
     if (serialExists) {
       showAlert("Este número de serie ya ha sido ingresado.", "error");
       return;
     }
-    
-    setItems([...items, {...inputs, ...checkboxes}])
-    setInputs({...inputs, serialNumber: ""})
-    setCheckboxes({isDemo: false, isBackup: false})
+
+    setItems([...items, { ...inputs }]);
+    setInputs({ ...inputs, serialNumber: "" });
     e.target["input-serial"].value = "";
-  }
+  };
 
   const handlePartNumberEntry = (e) => {
     e.preventDefault();
-    const entries = new Array(parseInt(e.target["input-cantidad"].value)).fill({...inputs, serialNumber: null, ...checkboxes})
-    setItems([...items, ...entries])
-    setCheckboxes({isDemo: false, isBackup: false})
+    const entries = new Array(parseInt(e.target["input-cantidad"].value)).fill({
+      ...inputs,
+      serialNumber: null,
+    });
+    setItems([...items, ...entries]);
     e.target["input-part-number"].value = "";
     e.target["input-descripcion"].value = "";
     e.target["input-cantidad"].value = 1;
-  }
+  };
 
   const handleItemDelete = (index) => {
-    setItems(items.filter( (item, i) => i != index))
-  }
+    setItems(items.filter((item, i) => i != index));
+  };
 
   const handleSubmit = async () => {
     setIsLoading(true);
     const response = await inventoryService.createEntry(items);
-    if(response.status === 'OK') {  
-      showAlert(response.message, 'success');
+    if (response.status === "OK") {
+      showAlert(response.message, "success");
       setInputs({
         proveedor: "",
         ordenDeCompra: "",
         facturaCompra: "",
-        cfdi: "",
         almacen: "",
         sector: "",
         partNumber: "",
@@ -116,25 +104,59 @@ const EntriesPage = () => {
       setItems([]);
       setIsLoading(false);
     } else {
-      showAlert(response.message, 'error');
+      showAlert(response.message, "error");
       setIsLoading(false);
     }
-  }
+  };
 
   const renderInputs = () => {
-    switch(activeTab) {
-      case "DATOS": 
+    switch (activeTab) {
+      case "DATOS":
         return (
           <form>
             <div className={styles.row}>
-              <Input label="proveedor" name="proveedor" id="input-proveedor" type="text" defaultValue={inputs.proveedor} onChange={(e) => handleInputChange(e)}/>
-              <Input label="orden de compra" name="ordenDeCompra" id="input-orden" type="text" defaultValue={inputs.ordenDeCompra} onChange={(e) => handleInputChange(e)}/>
-              <Input label="factura" name="facturaCompra" id="input-factura" type="text" defaultValue={inputs.facturaCompra} onChange={(e) => handleInputChange(e)}/>
-              <Input label="CFDI" name="cfdi" id="input-cfdi" type="text" defaultValue={inputs.cfdi} onChange={(e) => handleInputChange(e)}/>
+              <Input
+                label="proveedor"
+                name="proveedor"
+                id="input-proveedor"
+                type="text"
+                defaultValue={inputs.proveedor}
+                onChange={(e) => handleInputChange(e)}
+              />
+              <Input
+                label="orden de compra"
+                name="ordenDeCompra"
+                id="input-orden"
+                type="text"
+                defaultValue={inputs.ordenDeCompra}
+                onChange={(e) => handleInputChange(e)}
+              />
+              <Input
+                label="factura"
+                name="facturaCompra"
+                id="input-factura"
+                type="text"
+                defaultValue={inputs.facturaCompra}
+                onChange={(e) => handleInputChange(e)}
+              />
             </div>
             <div className={styles.row}>
-              <Input label="almacén" name="almacen" id="input-almacen" type="text" defaultValue={inputs.almacen} onChange={(e) => handleInputChange(e)}/>
-              <Input label="sector" name="sector" id="input-sector" type="text" defaultValue={inputs.sector} onChange={(e) => handleInputChange(e)}/>
+              <Input
+                label="almacén"
+                name="almacen"
+                id="input-almacen"
+                type="text"
+                defaultValue={inputs.almacen}
+                onChange={(e) => handleInputChange(e)}
+              />
+              <Input
+                label="sector"
+                name="sector"
+                id="input-sector"
+                type="text"
+                defaultValue={inputs.sector}
+                onChange={(e) => handleInputChange(e)}
+              />
             </div>
           </form>
         );
@@ -142,10 +164,31 @@ const EntriesPage = () => {
         return (
           <form onSubmit={handleSerialEntry}>
             <div className={styles.row}>
-              <Input label="part number" name="partNumber" id="input-part-number" type="text" required={true} onChange={(e) => handleInputChange(e)}/>
-              <Input label="descripción" name="descripcion" id="input-descripcion" type="text" required={true} onChange={(e) => handleInputChange(e)}/>
-              <Input label="serial number" name="serialNumber" id="input-serial" type="text" required={true} onChange={(e) => handleInputChange(e)}/>
-              <EnterButton isDisabled={isLoading}/>
+              <Input
+                label="part number"
+                name="partNumber"
+                id="input-part-number"
+                type="text"
+                required={true}
+                onChange={(e) => handleInputChange(e)}
+              />
+              <Input
+                label="descripción"
+                name="descripcion"
+                id="input-descripcion"
+                type="text"
+                required={true}
+                onChange={(e) => handleInputChange(e)}
+              />
+              <Input
+                label="serial number"
+                name="serialNumber"
+                id="input-serial"
+                type="text"
+                required={true}
+                onChange={(e) => handleInputChange(e)}
+              />
+              <EnterButton isDisabled={isLoading} />
             </div>
           </form>
         );
@@ -153,36 +196,74 @@ const EntriesPage = () => {
         return (
           <form onSubmit={handlePartNumberEntry}>
             <div className={styles.row}>
-              <Input label="part number" name="partNumber" id="input-part-number" type="text" required={true} onChange={(e) => handleInputChange(e)}/>
-              <Input label="descripción" name="descripcion" id="input-descripcion" type="text" required={true} onChange={(e) => handleInputChange(e)}/>
-              <Input inputStyle={{width: "30px"}} labelStyle={{width: "fit-content"}} label="cantidad" name="cantidad" id="input-cantidad" min={1} type="number" defaultValue={1} />
-              <EnterButton isDisabled={isLoading}/>
+              <Input
+                label="part number"
+                name="partNumber"
+                id="input-part-number"
+                type="text"
+                required={true}
+                onChange={(e) => handleInputChange(e)}
+              />
+              <Input
+                label="descripción"
+                name="descripcion"
+                id="input-descripcion"
+                type="text"
+                required={true}
+                onChange={(e) => handleInputChange(e)}
+              />
+              <Input
+                inputStyle={{ width: "30px" }}
+                labelStyle={{ width: "fit-content" }}
+                label="cantidad"
+                name="cantidad"
+                id="input-cantidad"
+                min={1}
+                type="number"
+                defaultValue={1}
+              />
+              <EnterButton isDisabled={isLoading} />
             </div>
           </form>
-        )
+        );
     }
-  }
+  };
 
   return (
     <MainLayout>
       <div className={styles.container}>
-        <FormFields tabs={tabs} renderInputs={renderInputs} activeTab={activeTab} handleChange={(tab) => handleTabChange(tab)} >
-          {activeTab !== 'DATOS' && <div className={styles.checkboxes}>
-            <label htmlFor="isDemo">
-              <input type="checkbox" name="isDemo" checked={checkboxes.isDemo} onChange={handleCheckboxChange} />
-              DEMO
-              </label>
-            <label htmlFor="isBackup">
-              <input type="checkbox" name="isBackup" checked={checkboxes.isBackup} onChange={handleCheckboxChange} />
-              BACKUP
-            </label>
-          </div>}
-        </FormFields>
-        <InventoryList attributes={["#", "Proveedor", "Orden de Compra", "Factura", "CFDI", "Almacén", "Sector", "Part Number", "Descripción", "Serial Number", "Demo", "Backup"]} items={items} mode="SHOW" onDelete={(index) => handleItemDelete(index)}/>
-        {isLoading ? <LoadingSpinner /> :<FinishButton disabled={isLoading} onFinish={() => handleSubmit()}/>}
+        <FormFields
+          tabs={tabs}
+          renderInputs={renderInputs}
+          activeTab={activeTab}
+          handleChange={(tab) => handleTabChange(tab)}
+        ></FormFields>
+        <InventoryList
+          attributes={[
+            "#",
+            "Proveedor",
+            "Orden de Compra",
+            "Factura",
+            "Almacén",
+            "Sector",
+            "Part Number",
+            "Descripción",
+            "Serial Number",
+            "Demo",
+            "Backup",
+          ]}
+          items={items}
+          mode="SHOW"
+          onDelete={(index) => handleItemDelete(index)}
+        />
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <FinishButton disabled={isLoading} onFinish={() => handleSubmit()} />
+        )}
       </div>
     </MainLayout>
-  )
-}
+  );
+};
 
-export default EntriesPage
+export default EntriesPage;
